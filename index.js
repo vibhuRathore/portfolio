a0da185ff89d8555828e3220346791cd7523d6e5
Vibhu Rathore <vibhurathore34@gmail.com>
2023-09-27 16:13:39 +0530

    boilerplate layout added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const ejsMate = require('ejs-mate');
 
 const app = express();
 
+
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views',path.join(__dirname,'views')) 
 
